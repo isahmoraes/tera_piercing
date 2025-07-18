eec46c06cdc4450415a8f1ad1ae39063bf11a4ee
admterapiercing <admterapiercing@gmail.com>
2025-07-18 19:32:00 +0000

    ajst

--- src/services/form.service.ts
+++ src/services/form.service.ts
@@ -1,9 +1,10 @@
 
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../enviroments/enviroment';
+
 import { Injectable } from '@angular/core';
 
 import { tap } from 'rxjs/operators';
+import { environment } from '../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class FormService {
   private baseUrl = environment.apiUrl;

41e7e60e5c5481bb80289a9857904191bce2b5be
unknown <isahmoraes8@gmail.com>
2025-07-21 20:06:51 -0300

    ajuste

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../enviroments/enviroment.prod';
+import { environment } from '../environments/environment.prod';
 import { Client } from '../models/client';
 @Injectable({
   providedIn: 'root'
--- src/services/form.service.ts
+++ src/services/form.service.ts
@@ -1,6 +1,6 @@
 
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 import { Injectable } from '@angular/core';
 
 import { tap } from 'rxjs/operators';
